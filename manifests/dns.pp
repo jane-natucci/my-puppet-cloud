1814f294bbbe7ad6452dd5d30a9af385a60459e6
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-02-27 11:09:18 +0100

    Fix.

--- manifests/dns.pp
+++ manifests/dns.pp
@@ -4,7 +4,7 @@ class cloud::dns {
     mode   => "0644",
     ensure => directory
   } -> file { "/home/git/.ssh/authorized_keys":
-    source => "puppet:///modules/hdp/authorized_keys",
+    source => "puppet:///modules/cloud/authorized_keys",
     owner  => "git",
     mode   => "0400",
     ensure => present
