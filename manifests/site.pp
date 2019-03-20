c239369dd69cf5011af981f9c988e90904667949
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-03-20 16:34:28 +0100

    Fix.

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,11 +49,11 @@ node 'ipa.natucci.de' {
 node 'jenkins.natucci.de' {
   class {'cloud::default':} ->
   class {'cloud::jenkins':} ->
-  class {'cloud::docker_config'}
+  class {'cloud::docker_config':}
 }
 
 node 'gitlab.natucci.de' {
   class {'cloud::default':} ->
   class {'cloud::gitlab':} ->
-  class {'cloud::docker_config'}
+  class {'cloud::docker_config':}
 }
