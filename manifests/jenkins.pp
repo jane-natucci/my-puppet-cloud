330a03dd66274215b9284cd9eb049dec2ffb9ec4
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-03-20 17:31:32 +0100

    Add docker config for jenkins user.

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -5,7 +5,7 @@ class cloud::jenkins {
     ensure => present,
   }
 
-  wget::fetch {'download ambari repo':
+  wget::fetch {'download jenkins repo':
     source      => 'https://pkg.jenkins.io/redhat-stable/jenkins.repo',
     destination => '/etc/yum.repos.d/jenkins.repo',
   } ->
@@ -32,5 +32,23 @@ class cloud::jenkins {
   exec {'firewall-cmd --add-rich-rule=\'rule family="ipv4" source address="116.203.70.215" accept\'':
     unless => 'firewall-cmd --query-rich-rule=\'rule family="ipv4" source address="116.203.70.215" accept\'',
     require => Service['firewalld'],
+  }
+
+  file {'/var/lib/jenkins/.docker':
+    ensure  => directory,
+    owner   => 'jenkins',
+    mode    => '0644',
+    require => Package['docker'],
+  }
+
+  file {'/var/lib/jenkins/.docker/config.json':
+    ensure  => present,
+    source  => 'puppet:///modules/cloud/config.json',
+    owner   => 'jenkins',
+    mode    => '0644',
+    require => [
+      File['/var/lib/jenkins/.docker/'],
+      Package['docker'],
+    ]
   }
 }
