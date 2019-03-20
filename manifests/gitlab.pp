f11324990710ece9cf28529381b289fefc303d4f
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-03-20 01:20:15 +0100

    Add gitlab.pp.

---
+++ manifests/gitlab.pp
@@ -0,0 +1,43 @@
+class cloud::gitlab {
+  Exec {path => '/usr/bin'}
+
+  package {'policycoreutils-python':
+    ensure => present,
+  }
+
+  package {'postfix':
+    ensure => present,
+  }
+
+  service {'postfix':
+    ensure  => running,
+    enable  => true,
+    require => Package['postfix'],
+  }
+
+  wget::fetch {'download gitlab repo':
+    source      => 'https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh',
+    destination => '/root/script.rpm.sh',
+  } ->
+  file {'/root/script.rpm.sh':
+    mode => '0744',
+  }
+
+  exec {'sh /root/script.rpm.sh':
+    require => File['/root/script.rpm.sh'],
+  } ->
+  exec {'yum install -y gitlab-ee':
+    environemnt => 'EXTERNAL_URL="https://gitlab.natucci.de"',
+    unless      => 'rpm -qa gitlab-ee',
+  }
+
+  service {'firewalld':
+    ensure => running,
+    enable => true,
+  }
+
+  exec {'firewall-cmd --add-service="http"',
+    unless  => 'firewall-cmd --query-service="http"',
+    require => Service['firewalld'],
+  }
+}
--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -2,10 +2,10 @@ class cloud::jenkins {
   Exec {path => '/usr/bin'}
 
   package {'java-1.8.0-openjdk.x86_64':
-    ensure  => present,
+    ensure => present,
   }
 
-  wget::fetch { 'download ambari repo':
+  wget::fetch {'download ambari repo':
     source      => 'https://pkg.jenkins.io/redhat-stable/jenkins.repo',
     destination => '/etc/yum.repos.d/jenkins.repo',
   } ->
