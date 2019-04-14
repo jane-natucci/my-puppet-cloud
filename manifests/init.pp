15411fa8665c288b6bf52c137815845e7c4b0874
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-04-14 06:03:31 +0200

    Add params class.

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,12 +42,6 @@
 #
 # Copyright 2019 Your name here, unless otherwise noted.
 #
-class cloud (
-  $ip_dns   = '5.9.18.7',
-  $ip_node1 = '176.9.124.9',
-  $ip_node2 = '148.251.12.145',
-  $ip_node3 = '136.243.43.100',
-  $ip_ipa   = '159.69.88.207',
-  $ip_vpn   = '37.252.248.93',
-) {
+
+class cloud () {
 }
