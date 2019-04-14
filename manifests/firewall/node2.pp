6987f2159c7125720acb35cd1b3b3e8a27324ec7
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-04-14 06:36:08 +0200

    Open port for MapReduce JobHistory Server Web UI on node2.

--- manifests/firewall/node2.pp
+++ manifests/firewall/node2.pp
@@ -30,6 +30,12 @@ class cloud::firewall::node2 () inherits ::cloud::params {
     require => Service['firewalld'],
   }
 
+  # MapReduce JobHistory Server Web UI
+  exec {"firewall-cmd --add-rich-rule=\"rule family=ipv4 source address=$::cloud::params::ip_vpn port port=19888 protocol=tcp accept\"":
+    unless  => "firewall-cmd --query-rich-rule=\"rule family=ipv4 source address=$::cloud::params::ip_vpn port port=19888 protocol=tcp accept\"",
+    require => Service['firewalld'],
+  }
+
   # everything from node1
   exec {"firewall-cmd --add-rich-rule=\"rule family=ipv4 source address=$::cloud::params::ip_node1 accept\"":
     unless  => "firewall-cmd --query-rich-rule=\"rule family=ipv4 source address=$::cloud::params::ip_node1 accept\"",
