5671a53297d0df95bad386d13c8f45588e2e7f4f
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-04-16 10:48:03 +0200

    Add requirement.

--- manifests/firewall/dns.pp
+++ manifests/firewall/dns.pp
@@ -13,47 +13,55 @@ class cloud::firewall::dns () inherits ::cloud::params {
     ensure  => present,
     source  => "${::cloud::params::ip_node1}",
     action  => 'accept',
+    Require => Service['firewalld'],
   }
 
   firewalld_rich_rule {'accept everything from node2':
     ensure  => present,
     source  => "${::cloud::params::ip_node2}",
     action  => 'accept',
+    Require => Service['firewalld'],
   }
 
   firewalld_rich_rule {'accept everything from node3':
     ensure  => present,
     source  => "${::cloud::params::ip_node3}",
     action  => 'accept',
+    Require => Service['firewalld'],
   }
 
   firewalld_rich_rule {'accept everything from ipa':
     ensure  => present,
     source  => "${::cloud::params::ip_ipa}",
     action  => 'accept',
+    Require => Service['firewalld'],
   }
 
   firewalld_rich_rule {'accept everything from gitlab':
     ensure  => present,
     source  => "${::cloud::params::ip_gitlab}",
     action  => 'accept',
+    Require => Service['firewalld'],
   }
 
   firewalld_rich_rule {'accept everything from jenkins':
     ensure  => present,
     source  => "${::cloud::params::ip_jenkins}",
     action  => 'accept',
+    Require => Service['firewalld'],
   }
 
   firewalld_rich_rule {'accept everything from natucci.de':
     ensure  => present,
     source  => "${::cloud::params::ip_natucci}",
     action  => 'accept',
+    Require => Service['firewalld'],
   }
 
   firewalld_rich_rule {'accept everything from VPN in Frankfurt':
     ensure  => present,
     source  => "${::cloud::params::ip_vpn}",
     action  => 'accept',
+    Require => Service['firewalld'],
   }
 }
