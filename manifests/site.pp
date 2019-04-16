e1c71e348e69f623dba670ac292eb31edb722066
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-04-16 10:43:16 +0200

    Specify full path to all classes in site.pp.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,50 +1,50 @@
 node 'dns.natucci.de' {
-  class {'cloud::dns':} ->
-  class {'cloud::default':} ->
-  class {'hdp::default':} ->
-  class {'hdp::server':} ->
-  class {'cloud::firewall::dns':}
+  class {'::cloud::dns':} ->
+  class {'::cloud::default':} ->
+  class {'::hdp::default':} ->
+  class {'::hdp::server':} ->
+  class {'::cloud::firewall::dns':}
 }
 
 node 'node1.natucci.de' {
-  class {'cloud::default':} ->
-  class {'hdp::default':} ->
-  class {'hdp::agent':} ->
-  class {'cloud::firewall::node1':}
+  class {'::cloud::default':} ->
+  class {'::hdp::default':} ->
+  class {'::hdp::agent':} ->
+  class {'::cloud::firewall::node1':}
 }
 
 node 'node2.natucci.de' {
-  class {'cloud::default':} ->
-  class {'hdp::default':} ->
-  class {'hdp::agent':} ->
-  class {'cloud::firewall::node2':}
+  class {'::cloud::default':} ->
+  class {'::hdp::default':} ->
+  class {'::hdp::agent':} ->
+  class {'::cloud::firewall::node2':}
 }
 
 node 'node3.natucci.de' {
-  class {'cloud::default':} ->
-  class {'hdp::default':} ->
-  class {'hdp::agent':} ->
-  class {'cloud::firewall::node3':}
+  class {'::cloud::default':} ->
+  class {'::hdp::default':} ->
+  class {'::hdp::agent':} ->
+  class {'::cloud::firewall::node3':}
 }
 
 node 'ipa.natucci.de' {
-  class {'cloud::default':}
+  class {'::cloud::default':}
 }
 
 node 'jenkins.natucci.de' {
-  class {'cloud::default':} ->
-  class {'cloud::jenkins':} ->
-  class {'cloud::docker_config':}
+  class {'::cloud::default':} ->
+  class {'::cloud::jenkins':} ->
+  class {'::cloud::docker_config':}
 }
 
 node 'gitlab.natucci.de' {
-  class {'cloud::default':} ->
-  class {'cloud::gitlab':} ->
-  class {'cloud::docker_config':}
+  class {'::cloud::default':} ->
+  class {'::cloud::gitlab':} ->
+  class {'::cloud::docker_config':}
 }
 
 node 'natucci.de' {
-  class {'cloud::default':} ->
-  class {'cloud::docker_config':} ->
-  class {'cloud::natucci_de':}
+  class {'::cloud::default':} ->
+  class {'::cloud::docker_config':} ->
+  class {'::cloud::natucci_de':}
 }
