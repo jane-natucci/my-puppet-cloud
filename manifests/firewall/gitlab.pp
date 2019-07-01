f868b6866c803abf070f75bd7a923b9c63267e14
Eugen Prokhorenko <eugen.prokhorenko@gmail.com>
2019-07-01 04:27:36 +0200

    Allow connections from everywhere.

--- manifests/firewall/gitlab.pp
+++ manifests/firewall/gitlab.pp
@@ -9,28 +9,4 @@ class cloud::firewall::gitlab () inherits ::cloud::params {
     service => 'ssh',
     require => Service['firewalld'],
   }
-
-  firewalld_rich_rule {'accept http from VPN in Frankfurt':
-    ensure  => present,
-    source  => "${::cloud::params::ip_vpn}",
-    action  => 'accept',
-    service => 'http',
-    require => Service['firewalld'],
-  }
-
-  firewalld_rich_rule {'accept https from VPN in Frankfurt':
-    ensure  => present,
-    source  => "${::cloud::params::ip_vpn}",
-    action  => 'accept',
-    service => 'https',
-    require => Service['firewalld'],
-  }
-
-  firewalld_rich_rule {'accept https from VPN in London':
-    ensure  => present,
-    source  => "51.77.126.243",
-    action  => 'accept',
-    service => 'https',
-    require => Service['firewalld'],
-  }
 }
