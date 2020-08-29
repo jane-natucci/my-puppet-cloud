90bc28ed9d4cb7297fa5dc1a375f4d7b3578f261
Eugen Natuchchi <eugen@globaldatanet.com>
2020-08-29 22:41:35 +0300

    Nodes.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,11 @@
+node 'www.natucci.de' {
+  class {'::cloud::default':} ->
+  class {'::cloud::docker_config':} ->
+  class {'::cloud::natucci_de':}
+}
+
+/*
+
 node 'dns.natucci.de' {
   class {'::cloud::dns':} ->
   class {'::cloud::default':} ->
@@ -7,57 +15,10 @@ node 'dns.natucci.de' {
   class {'::cloud::docker_config':}
 }
 
-/*
-
-node 'node1.natucci.de' {
-  class {'::cloud::default':} ->
-  class {'::hdp::default':} ->
-  class {'::hdp::agent':} ->
-  class {'::cloud::firewall::node1':}
-}
-
-node 'node2.natucci.de' {
-  class {'::cloud::default':} ->
-  class {'::hdp::default':} ->
-  class {'::hdp::agent':} ->
-  class {'::cloud::firewall::node2':}
-}
-
-node 'node3.natucci.de' {
-  class {'::cloud::default':} ->
-  class {'::hdp::default':} ->
-  class {'::hdp::agent':} ->
-  class {'::cloud::firewall::node3':}
-}
-
-node 'ipa.natucci.de' {
-  class {'::cloud::default':} ->
-  class {'::cloud::firewall::ipa':}
-}
-
 node 'jenkins.natucci.de' {
   class {'::cloud::default':} ->
   class {'::cloud::jenkins':} ->
   class {'::cloud::docker_config':}
-}
-
-node 'gitlab.natucci.de' {
-  class {'::cloud::default':} ->
-  class {'::cloud::gitlab':} ->
-  class {'::cloud::docker_config':} ->
-  class {'::cloud::firewall::gitlab':}
-}
-
-node 'natucci.de' {
-  class {'::cloud::default':} ->
-  class {'::cloud::docker_config':} ->
-  class {'::cloud::natucci_de':}
-}
-
-node 'workstation.natucci.de' {
-  class {'::cloud::default':} ->
-  class {'::cloud::docker_config':} ->
-  class {'::cloud::workstation':}
 }
 
 */
