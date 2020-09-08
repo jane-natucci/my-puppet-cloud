1fe679f8652c5b3b4f982d6812f6aedc2136c236
Eugen Natuchchi <eugen@globaldatanet.com>
2020-09-08 09:35:17 +0300

    Remove old files.

--- manifests/default.pp
+++ manifests/default.pp
@@ -53,4 +53,15 @@ class cloud::default {
     ensure => present,
     require => File['/root/.ssh/authorized_keys']
   }
+
+    wget::fetch {'download gitlab repo':
+    source      => 'https://raw.githubusercontent.com/GitAlias/gitalias/master/gitalias.txt',
+    destination => '/root/gitalias.txt'
+  }
+
+  file {'/root/.gitconfig':
+    source => 'puppet:///modules/cloud/.gitconfig',
+    owner  => 0,
+    ensure => present,
+  }
 }
