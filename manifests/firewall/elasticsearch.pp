118b7503a4c3529cfce2237784d269bdfad7ccdd
Eugen Natuchchi <eugen@globaldatanet.com>
2020-10-03 01:07:37 +0200

    Remove useless firewall rule.

--- manifests/firewall/elasticsearch.pp
+++ manifests/firewall/elasticsearch.pp
@@ -10,17 +10,6 @@ class cloud::firewall::elasticsearch () inherits ::cloud::params {
     require  => Service['firewalld'],
   }
 
-  firewalld_rich_rule {'Port 9200 for home IP':
-    ensure   => present,
-    source   => "$::cloud::params::home_ip",
-    port     => {
-      port => '9200',
-      protocol => 'tcp',
-    },
-    action   => 'accept',
-    require  => Service['firewalld'],
-  }
-
   # kibana
   firewalld_rich_rule {'Port 5601 for home IP':
     ensure   => present,
