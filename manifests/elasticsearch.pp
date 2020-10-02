8a04e744108c1b9be7f2c7066c9ce5c2a9458b57
Eugen Natuchchi <eugen@globaldatanet.com>
2020-10-03 00:36:27 +0200

    Requirements.

--- manifests/elasticsearch.pp
+++ manifests/elasticsearch.pp
@@ -12,50 +12,69 @@ class cloud::elasticsearch {
   package {'elasticsearch':
     require => File['/etc/yum.repos.d/elasticsearch.repo'],
     ensure  => present,
-  } ->
-  package {'kibana':
-    require => File['/etc/yum.repos.d/elasticsearch.repo'],
-    ensure  => present,
-  } ->
+  }
+  
   file {'/elasticsearch':
     ensure => directory,
     owner  => 'elasticsearch',
     mode   => '0755'
-  } -> 
+  }
+  
   file {'/elasticsearch/log':
     ensure => directory,
     owner  => 'elasticsearch',
-    mode   => '0755'
-  } ->
+    mode   => '0755',
+    require => File['/elasticsearch'],
+  }
+
   file {'/elasticsearch/data':
     ensure => directory,
     owner  => 'elasticsearch',
-    mode   => '0755'
-  } ->
+    mode   => '0755',
+    require => File['/elasticsearch'],
+  }
+
   file {'/etc/elasticsearch/jvm.options.d/heapsize.options':
     ensure => file,
     source => 'puppet:///modules/cloud/heapsize.options',
     owner  => 'elasticsearch',
     mode   => '0660'
-  } ->
+  }
+
   file {'/etc/elasticsearch/elasticsearch.yml':
     ensure  => file,
     content => template('cloud/elasticsearch.yml.erb'),
     owner   => 'elasticsearch',
     mode    => '0660',
-  } ->
+    require => File['/elasticsearch/log'],
+    require => File['/elasticsearch/data'],
+  }
+
+  service {'elasticsearch':
+    ensure => running,
+    enable => true,
+    require => File['/etc/elasticsearch/elasticsearch.yml'],
+    require => File['/etc/elasticsearch/jvm.options.d/heapsize.options'],
+    require => Package['elasticsearch'],
+  }
+  
+  package {'kibana':
+    require => File['/etc/yum.repos.d/elasticsearch.repo'],
+    ensure  => present,
+  }
+
   file {'/etc/elasticsearch/kibana.yml':
     ensure  => file,
     content => template('cloud/kibana.yml.erb'),
     owner   => 'kibana',
     mode    => '0660',
-  } ->
-  service {'elasticsearch':
-    ensure => running,
-    enable => true,
-  } ->
+    require => Package['kibana'],
+  }
+  
   service {'kibana':
     ensure => running,
     enable => true,
+    require => Package['kibana'],
+    require => File['/etc/elasticsearch/kibana.yml'],
   }
 }
