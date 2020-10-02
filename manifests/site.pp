6ef0663e87d5db5502454eacee8954d86da6a060
Eugen Natuchchi <eugen@globaldatanet.com>
2020-10-02 15:05:24 +0200

    Fix puppet class for elasticsearch.

--- manifests/site.pp
+++ manifests/site.pp
@@ -15,5 +15,5 @@ node 'puppet.natucci.de' {
 node 'elasticsearch.natucci.de' {
   class {'::cloud::default':} ->
   class {'::cloud::elasticsearch':} ->
-  class {'::cloud::firewall::elastic':}
+  class {'::cloud::firewall::elasticsearch':}
 }
