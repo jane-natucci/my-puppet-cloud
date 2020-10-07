00547078063ab52fa42ab5b6b7ca385d77341ea8
Eugen Natuchchi <eugen@globaldatanet.com>
2020-10-07 06:25:22 +0200

    Docker storage.

--- manifests/openshift.pp
+++ manifests/openshift.pp
@@ -10,7 +10,7 @@ class cloud::openshift {
     'psacct',
     'python-ipaddress',
     'docker',
-    'centos-release-openshift-origin',
+    'centos-release-openshift-origin39',
     ]
 
   package {$dependencies:
@@ -21,5 +21,45 @@ class cloud::openshift {
     ensure  => running,
     enable  => true,
     require => Package['NetworkManager'],
+  }
+
+  physical_volume {'/dev/sdb':
+    ensure  => present,
+    require => Package['docker']
+  }
+  -> volume_group {'docker':
+    ensure           => present,
+    physical_volumes => '/dev/sdb',
+  }
+  -> logical_volume {'thinpool':
+    ensure       => present,
+    volume_group => 'docker',
+    extents      => '90%VG',
+  }
+  -> logical_volume {'thinpoolmeta':
+    ensure       => present,
+    volume_group => 'docker',
+    extents      => '5%VG',
+  }
+  -> exec {'lvconvert -y --zero n -c 512K --thinpool docker/thinpool --poolmetadata docker/thinpoolmeta':
+    path => '/usr/sbin'
+  }
+  -> file {'/etc/lvm/profile/docker-thinpool.profile':
+    ensure  => present,
+    content => 'activation {\n thin_pool_autoextend_threshold = 75\n thin_pool_autoextend_percent = 25\n}',
+    owner   => 0,
+    mode    => '0644',
+  }
+  -> exec {'lvchange --metadataprofile docker-thinpool docker/thinpool':
+    path => '/usr/sbin',
+  }
+  -> file {'/etc/docker/daemon.json':
+    ensure => present,
+    source => 'puppet:///modules/cloud/daemon.json',
+    owner  => 0,
+    mode   => '0644',
+  }
+  -> exec {'rm -rf /etc/sysconfig/docker-storage*':
+    path => '/usr/bin',
   }
 }
