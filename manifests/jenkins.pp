13ea141c3fd3c1fc7bf7b1802410a59ff60f1613
Eugen Natucci <eugen.natucci@gmail.com>
2020-11-14 13:36:00 +0100

    Psql config.

--- manifests/jenkins.pp
+++ manifests/jenkins.pp
@@ -41,6 +41,7 @@ class cloud::jenkins {
   }
 
   class { 'postgresql::server':
-    ip_mask_allow_all_users => '0.0.0.0/0',
+    ip_mask_deny_postgres_user => '0.0.0.0/32',
+    ip_mask_allow_all_users    => '0.0.0.0/0',
   }
 }
